5871f5dbf85e9c3880266f0b2f55402a497c8757
uryellll <ffurio.uriel@gmail.com>
2021-06-07 22:20:01 +0800

    fix api call on refresh

--- random quote generator/src/App.tsx
+++ random quote generator/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-interface quotesArr {
+interface QuotesArr {
 	quote: string;
 	author: string;
 }
@@ -11,17 +11,16 @@ function App() {
 	const API_URL =
 		"https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
-	const [quotesArr, setQuotesArr] = useState<Array<quotesArr>>([]);
-	const [loading, setLoading] = useState(false);
+	const [quotesArr, setQuotesArr] = useState<Array<QuotesArr>>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [quoteNumber, setQuoteNumber] = useState<number>(0);
 
-	async function getQuote() {
-		setLoading(true);
+	async function fetchApi() {
 		await axios
 			.get(API_URL)
 			.then((res) => {
-				console.log(res.data.quotes);
 				setQuotesArr(res.data.quotes);
-				setLoading(false);
+				setIsLoading(false);
 			})
 			.catch((err) => {
 				console.log(err);
@@ -29,18 +28,29 @@ function App() {
 	}
 
 	useEffect(() => {
-		getQuote();
+		setIsLoading(true);
+		fetchApi();
 	}, []);
+
+	function getRandomQuote() {
+		setQuoteNumber(Math.floor(Math.random() * 101));
+	}
+
+	if (isLoading) {
+		return <div className="App">Loading...</div>;
+	}
 
 	return (
 		<div className="App">
 			<div className="quote-box" id="quote-box">
 				<blockquote id="text">
-					<p>{quotesArr[0].quote}</p>
+					<p>{quotesArr[quoteNumber].quote}</p>
 				</blockquote>
-				<p id="author">- {quotesArr[0].author}</p>
+				<p id="author">- {quotesArr[quoteNumber].author}</p>
 				<div className="links">
-					<button id="new-quote">New Qoute</button>
+					<button id="new-quote" onClick={getRandomQuote}>
+						New Quote
+					</button>
 					<a href="twitter.com/intent/tweet" id="tweet-quote">
 						tweet-quote
 					</a>
